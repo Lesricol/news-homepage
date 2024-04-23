5ab5e7c5679443fdf3f6f9196a803982f3ca2eb1
Victor <lessricc@gmail.com>
2024-04-23 21:54:23 +0200

    vamos bien

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,15 @@
 // El styles lo importamos aquí para que se encargue Vite de compilar todo
 import '../scss/styles.scss';
+
+const buttonElement = document.getElementById('button-menu');
+const menuElement = document.getElementById('menu');
+
+const handleMenu = () => {
+  if (menuElement.classList.contains('show')) {
+    menuElement.classList.remove('show');
+  } else {
+    menuElement.classList.add('show');
+  }
+};
+
+buttonElement.addEventListener('click', handleMenu);
