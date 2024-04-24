984020af7e49e7a2c23d6b3f3cc3274f59b0b0e0
Victor <lessricc@gmail.com>
2024-04-24 21:34:30 +0200

    termine

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,20 @@
 // El styles lo importamos aquí para que se encargue Vite de compilar todo
 import '../scss/styles.scss';
+import imageClose from '../assets/images/icon-menu-close.svg';
+import imageMenu from '../assets/images/icon-menu.svg';
 
 const buttonElement = document.getElementById('button-menu');
 const menuElement = document.getElementById('menu');
 
 const handleMenu = () => {
-  if (menuElement.classList.contains('show')) {
-    menuElement.classList.remove('show');
+  if (menuElement.classList.contains('menu--show')) {
+    menuElement.classList.remove('menu--show');
+    document.body.classList.remove('bg-opacity');
+    buttonElement.src = imageMenu;
   } else {
-    menuElement.classList.add('show');
+    menuElement.classList.add('menu--show');
+    document.body.classList.add('bg-opacity');
+    buttonElement.src = imageClose;
   }
 };
 
